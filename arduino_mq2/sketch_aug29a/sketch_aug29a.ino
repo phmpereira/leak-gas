97150bdf4006fd8cbeacb37cbde3e5394024ce67
phmpereira <pedro.pereira@sptech.school>
2024-09-10 21:30:55 -0300

    Adicionada calculadora e codigo do arduino

---
+++ arduino_mq2/sketch_aug29a/sketch_aug29a.ino
@@ -0,0 +1,41 @@
+// Declarando o pino analógico que o sensor está conectado no Arduino UNO
+const int PINO_MQ2 = A2;
+
+// Declarando valores mínimos e máximos de leitura
+const int VALOR_MINIMO = 100;
+const int VALOR_MAXIMO = 1000;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  // Lendo os dados do sensor e atribuindo à uma variável
+  int valor_sensor = analogRead(PINO_MQ2);
+
+  // Conversão da leitura para porcentagem
+  float porcentagem = ((float)(valor_sensor - VALOR_MINIMO) / (VALOR_MAXIMO - VALOR_MINIMO)) * 100;
+
+  // Fazendo alguns tratamentos de dados, para caso a conversão dê 
+  // menos que 0% (nesse caso setamos a porcentagem para 0)
+  // ou mais que 100% (nesse caso setamos a porcentagem para 100)
+  // uma vez que não existe nem porcentagem negativa, nem maior que 100
+  if(porcentagem < 0){
+    porcentagem = 0;
+  }else if(porcentagem > 100){
+    porcentagem = 100;
+  }
+
+  // Exibindo as mensagens com os valores
+  Serial.print("Gás Mínimo:");
+  Serial.print(5);
+  Serial.print(",");
+  Serial.print("Gás Máximo:");
+  Serial.print(15);
+  Serial.print(",");
+  Serial.print("Gás Atual:");
+  Serial.println(porcentagem);
+
+  // Dando um intervalo de 2s (2000ms) para que a função loop() seja executada novamente
+  delay(500);
+}
